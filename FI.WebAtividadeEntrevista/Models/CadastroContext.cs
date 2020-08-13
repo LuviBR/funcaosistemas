3ed058bb7bc6f50e85a254b80b46699164e6f36b
Lucas Viana <lucas_viana99@outlook.com>
2020-08-13 00:12:55 -0300

    enviar banco de dados

---
+++ FI.WebAtividadeEntrevista/Models/CadastroContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebAtividadeEntrevista.Models
+{
+	public class CadastroContext : DbContext
+	{
+		public CadastroContext() : base("BancoDeDados")
+		{
+		}
+
+		public DbSet<ClienteModel> Clientes { get; set; }
+	}
+}
